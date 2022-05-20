4c65bce900e550c4f1d24279a6bd980c8ce2c69c
Josue Lubaki <josuelubaki@gmail.com>
2022-05-20 19:14:35 -0400

    initial project

---
+++ kafka-basics/src/main/java/ca/josue/demo/kafka/ProducerDemo.java
@@ -0,0 +1,12 @@
+package ca.josue.demo.kafka;
+
+/**
+ * @author Josue Lubaki
+ * @version 1.0
+ * @since 2022-05-20
+ */
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
