6180ad48a607bf0edb829d53762fe0c73b194845
Josue Lubaki <josuelubaki@gmail.com>
2022-05-22 04:38:58 -0400

    config safe producer for kafka version <= 2.8.0

--- Kafka-producer-wikimedia/src/main/java/ca/josue/demo/kafka/wikimedia/WikimediaChangesProducer.java
+++ Kafka-producer-wikimedia/src/main/java/ca/josue/demo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -28,6 +28,12 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // set safe properties for version 2.8.0 and lower
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         // create a producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
