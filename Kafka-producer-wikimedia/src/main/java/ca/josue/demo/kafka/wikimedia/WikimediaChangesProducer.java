0a5d342a43f7de14f7d73a329d633409fec1d006
Josue Lubaki <josuelubaki@gmail.com>
2022-05-22 06:31:58 -0400

    added compression & batch memory & linger time

--- Kafka-producer-wikimedia/src/main/java/ca/josue/demo/kafka/wikimedia/WikimediaChangesProducer.java
+++ Kafka-producer-wikimedia/src/main/java/ca/josue/demo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -34,6 +34,11 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         // create a producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
