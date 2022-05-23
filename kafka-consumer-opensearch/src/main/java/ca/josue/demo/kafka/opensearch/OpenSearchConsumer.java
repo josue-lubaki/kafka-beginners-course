b60b9f92c4a7febb58ac816c025c436d5b2d4b42
Josue Lubaki <josuelubaki@gmail.com>
2022-05-23 00:29:44 -0400

    init opensearch project

---
+++ kafka-consumer-opensearch/src/main/java/ca/josue/demo/kafka/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,12 @@
+package ca.josue.demo.kafka.opensearch;
+
+/**
+ * @author Josue Lubaki
+ * @version 1.0
+ * @since 2022-05-23
+ */
+public class OpenSearchConsumer {
+    public static void main(String[] args) {
+
+    }
+}
